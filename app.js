1ce36a16c9f95cc2784ce9d5cb5315a6804b2410
tora0001 <tobe.2309@hotmail.com>
2023-05-10 21:30:06 +0200

    member form added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const endpoint = "https://delfinen-36fde-default-rtdb.firebaseio.com/";
 function initApp() {
   console.log("Delfinen svømmer!");
   getMembers();
+  document.querySelector(".new-member-btn").addEventListener("click", addMemberClicked);
 }
 
 async function getMembers() {
@@ -26,4 +27,70 @@ function prepareMemberData(memberObject) {
   }
   console.log(memberArray);
   return memberArray;
+}
+
+function addMemberClicked() {
+  document.querySelector("#add-member-form").showModal();
+  const newMemberForm = /*html*/ `
+    <form id="new-member-form" method="dialog">
+    <h1>Opret nyt medlem</h1>
+    <label for="name">Navn:</label>
+    <input type="text" id="name" name="name" required placeholder="Indtast navn på medlem" />
+    <br>
+    <br>
+    <label for="age">Alder:</label>
+    <input type="text" id="age" name="age" required placeholder="Indtast alder på medlem" />
+    <br>
+    <br>
+    <label for="activity">Aktivitetsform:</label>
+    <select id="activity" required>
+      <option value="" selected>ikke valgt</option>
+      <option value="competition">Konkurrencesvømmer</option>
+      <option value="exercise">Motionist</option>
+    </select>
+    <br>
+    <br>
+    <label for="team">Hold:</label>
+    <select id="team" required>
+      <option value="" selected>ikke valgt</option>
+      <option value="junior">Junior (under 18)</option>
+      <option value="senior">Senior (18 og over)</option>
+    </select>
+    <br>
+    <br>
+    <label for="disciplin">Disciplin:</label>
+    <select id="disciplin" required>
+      <option value="" selected>ikke valgt</option>
+      <option value="breast">Brystsvømning</option>
+      <option value="butterfly">Butterfly</option>
+      <option value="crawl">Crawl</option>
+      <option value="backcrawl">Rygcrawl</option>
+    </select>
+    <br>
+    <br>
+    <label for="subscription">Medlemskab:</label>
+    <select id="subscription" required>
+      <option value="" selected>ikke valgt</option>
+      <option value="young">Ungdomssvømmer (under 18)</option>
+      <option value="senior">Seniorsvømmer (18 og over)</option>
+      <option value="old">Pensionist (60 og over)</option>
+      <option value="passive">Passivt medlemskab</option>
+    </select>
+    <br>
+    <br>
+    <button type="submit" value="submit">Opret</button>
+    <input type="button" id="btn-cancel" value="Luk">
+    </form>
+    `;
+  document.querySelector("#add-member-form").innerHTML = newMemberForm;
+
+  document.querySelector("#add-member-form").addEventListener("submit", prepareNewMember);
+
+  document.querySelector("#btn-cancel").addEventListener("click", () => {
+    document.querySelector("#add-member-form").close();
+  });
+}
+
+async function prepareNewMember() {
+  console.log("nyt medlem oprettet!");
 }
